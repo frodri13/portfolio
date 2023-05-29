2b16c9aa1b2f3cc9397625df1b62728aaa8cfca8
Fabio Rodriguez <alessf13@gmail.com>
2023-05-29 14:42:20 +0100

    opacity for footer dark mode

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className='container max-w-screen-lg mx-auto'>
                <Icons />       
             </div>
-            <p className="text-sm mt-2  opacity-50">
+            <p className="text-sm mt-2  opacity-50 dark:opacity-80">
 				&copy; {new Date().getFullYear()} Fabio Rodriguez. All rights reserved.
 			</p>
         </div>
