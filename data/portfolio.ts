ea515a09cb8aae70005ee34169fcee7cb3334633
Fabio Rodriguez <alessf13@gmail.com>
2023-05-29 14:55:35 +0100

    projects boilerplate

---
+++ data/portfolio.ts
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        title: 'sth',
+        imgUrl: 'src',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https:',
+    },
+    {
+        title: 'sth',
+        imgUrl: 'src',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https:',
+    }
+]
+
+export default projects
